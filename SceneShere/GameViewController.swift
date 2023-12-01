b4251713f9be79d38c2a9fb12a08025ab5f0f986
zabaroVladimir <zabarik@icloud.com>
2023-12-01 14:22:21 +0100

    firsC

---
+++ SceneShere/GameViewController.swift
@@ -0,0 +1,28 @@
+//
+//  GameViewController.swift
+//  SceneShere
+//
+//  Created by Vladimir Zabaro on 25/11/23.
+//
+
+import UIKit
+import SceneKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        
+    }
+    
+    override var shouldAutorotate: Bool {
+        return false
+    }
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
